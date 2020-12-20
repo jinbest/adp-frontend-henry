fd3133e30ced109500449dafbf00945f3528d233
Jin Gang <jgang@AppleDream.local>
2020-12-20 21:59:26 +0100

    section4

--- src/pages/home/Section4.tsx
+++ src/pages/home/Section4.tsx
@@ -11,32 +11,55 @@ const Section4 = ({subDomain}: Props) => {
   const data = require(`../../assets/${subDomain}/Database`);
 
   return (
-    <section className='mg-t-7 Container'>
-      <Grid container item xs={12}>
-        <Grid item xs={6}>
-          <Typography className='f22'>
-            Welcome to DeviceList, Canada’s mobile device marketplace
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Grid item container xs={12}>
-            {data.devicelistData.slice(0,2).map((item:any, index:number) => {
-              return (
-                <Grid item xs={6}>
-                  <Box className='cart-device-list'>
-                    <DeviceListComponent img={item.img} title={item.title} content={item.content} key={index} />
-                  </Box>
-                </Grid>
-              )
-            })}
+    <section className='Container'>
+      <Box className='mg-t-7'>
+        <Grid container item xs={12}>
+          <Grid item xs={12} sm={6}>
+            <Typography className='f40 bold section4-title'>
+              Welcome to DeviceList, Canada’s mobile device marketplace
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Grid item container xs={12}>
+              {data.devicelistData.slice(0,2).map((item:any, index:number) => {
+                return (
+                  <Grid item xs={12} sm={6}>
+                    <Box className='cart-device-list'>
+                      <DeviceListComponent img={item.img} title={item.title} content={item.content} key={index} />
+                    </Box>
+                  </Grid>
+                )
+              })}
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Box className='section4-button desktop'>
+              <Button title='Our quality charter' bgcolor='#054DFA' borderR='20px'/>
+            </Box>
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Box className='section4-button'>
-            <Button title='Our quality charter' bgcolor='#054DFA' borderR='20px'/>
-          </Box>
+        <Grid container item xs={12}>
+          <Grid item xs={12} sm={4} ></Grid>
+          <Grid item xs={12} sm={8} >
+            <Grid item container xs={12}>
+              {data.devicelistData.slice(2,5).map((item:any, index:number) => {
+                return (
+                  <Grid item xs={12} sm={4}>
+                    <Box className='cart-device-list'>
+                      <DeviceListComponent img={item.img} title={item.title} content={item.content} key={index} />
+                    </Box>
+                  </Grid>
+                )
+              })}
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Box className='section4-button mobile'>
+              <Button title='Our quality charter' bgcolor='#054DFA' borderR='20px'/>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </section>
   )
 }

9f7970e0d16ee3b0465f395077285f6fb3be72fe
henry <wxiao1214@gmail.com>
2020-12-21 17:50:36 +0800

    sec6

--- src/pages/home/Section6.tsx
+++ src/pages/home/Section6.tsx
@@ -21,8 +21,8 @@ const Section6 = ({subDomain}: Props) => {
       <Grid container item xs={12} spacing={3} className='sec6-card'>
         {data.whyCustomersData.map((item:any, index:number) => {
           return (
-            <Grid item xs={12} sm={6} md={4}>
-              <Box className=''>
+            <Grid item xs={12} md={4}>
+              <Box className='sec6-card'>
                 <CardWhyCustomer key={index} score={item.score} days={item.days} content={item.content} reviewer={item.reviewer} />
               </Box>
             </Grid>

d8cd076c4ea7e0cb1d1b47eb9937af50bcfbb03d
henry <wxiao1214@gmail.com>
2020-12-21 18:00:13 +0800

    asdfasdf

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 const Home = ({subDomain}: Props) => {
-  const SectionItemComponents = [Section1, Section3, Section4, Section5, Section6]
+  const SectionItemComponents = [Section1, Section2, Section3, Section4, Section5, Section6]
   
   return (
     <div>
